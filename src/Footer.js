e1569bf3dab2257550d80c00055a76bf6ee19cb3
chen mishani <chen.mishani@gmail.com>
2024-06-09 21:03:57 +0300

    First commite

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; ALL IN ONE 2024 - All functions in one website.</p>
+      <p>&copy; Tools For All 2024 - All You Need In One Website.</p>
     </footer>
   );
 };
